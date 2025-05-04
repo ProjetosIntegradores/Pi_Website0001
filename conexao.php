101088fed56c755ea564086a9eefdccbbf16a120
ProjetosIntegradores <pisurfbrits@gmail.com>
2025-05-04 13:52:10 -0300

    sad
    not working

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 $db_host = 'localhost'; // Database host
-$db_username = 'root'; // Database username
-$db_password = 'a1s2d3f4'; // Database password
-$db_Name = 'formulario_clientes'; // Database name
-$conexao = new mysqli($db_host, $db_username, $db_password, $db_Name);
+$db_username = '<username>'; // Database username
+$db_password = '<password>'; // Database password
+$db_Name = 'formulario'; // Database name
+$conexao = new mysqli( $db_host, $db_username, $db_password, $db_Name);
 if ($conexao->connect_error) {
     die("Connection failed: " . $conexao->connect_error);
 } else {
