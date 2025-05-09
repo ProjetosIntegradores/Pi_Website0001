49aee7d0a099d5a6973c1597e05e5c985e40b797
ProjetosIntegradores <pisurfbrits@gmail.com>
2025-05-08 21:07:44 -0300

    to confuso
    ainda bem que tenho backup das minha coisas

---
+++ TeladeLogin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+<body>
+    <div class="tela-login">
+        <h1>Login</h1>
+        <form action="processa_login.php" method="POST">
+         <input type="text" name="usuario" placeholder="Usuário"> 
+        <br><br>
+         <input type="password" name= "senha" placeholder="Senha"> 
+        <br><br>
+        <input class="inputsubmit" type="submit" name="submit" value="Entrar">
+        <br><br>
+        <button onclick="window.location.href='PaginaInicial.html';">Voltar</button>
+    </div>
+</body>
+</html>
